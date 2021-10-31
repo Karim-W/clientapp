f3d80c87fe574d645464888d0099ef66b025b701
CX-Karim <karimh@cxunicorn.com>
2021-10-31 16:53:49 +0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { toast } from "react-hot-toast";
+import { TeamsToast } from "./components/Toasts/TeamsToast";
+import CardExample from "./components/cardExample/cardExample";
+import React, { useEffect, useMemo, useContext, useState } from "react";
+import { AuthContext } from "./contexts/authContext";
+import { Button } from "@fluentui/react-northstar";
+import { getRandomUser } from "./apis/demoApi";
 
 function App() {
+  const [authToken, setAuthToken] = useState();
+  const AuthValue = useMemo(
+    () => ({ authToken, setAuthToken }),
+    [authToken, setAuthToken]
+  );
+  React.useEffect(() => {
+    console.log("App.js is running");
+    toast.error("Error message");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={AuthValue}>
+      <div className="App">
+        <Button
+          content="hey"
+          onClick={async () => {
+            const user = await getRandomUser();
+            console.log(user);
+          }}
+        />
+        <CardExample />
+        <TeamsToast />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
